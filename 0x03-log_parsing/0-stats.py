44f915c110c9d25603c30c5a3882e5da2eb7ea30
Mo2men-dev <momensameh5@gmail.com>
2024-08-16 21:49:12 +0300

    log parsing: fixed styling

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
+"""Log parsing"""
+
+
 import sys
 import re
 
+
 def log_parsing():
-    reg = re.compile(r"(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\] \"GET /projects/260 HTTP/1.1\" (\d+) (\d+)")
+    """Log parsing"""
+    reg = re.compile(r"""(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\] \"GET /projects/260 HTTP/1.1\" (\d+) (\d+)""")  # noqa
     total_size = 0
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    status_codes = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    }
     line_count = 0
 
     for line in sys.stdin:
@@ -24,11 +38,12 @@ def log_parsing():
             for key, value in sorted(status_codes.items()):
                 if value:
                     print("{}: {}".format(key, value))
-        
+
     print("File size: {}".format(total_size))
     for key, value in sorted(status_codes.items()):
         if value:
             print("{}: {}".format(key, value))
+
 
 if __name__ == "__main__":
     log_parsing()
