53ae97fad5bff5d58685fc84606133bfb7c28742
Momen Sameh <95236373+Mo2men-dev@users.noreply.github.com>
2024-09-23 16:20:32 +0300

    making change: ok i give up

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,43 +1,25 @@
 #!/usr/bin/python3
-"""
-Making Change
-"""
-
-
-def fill_arr(amnt):
-    """
-    Function to pre-fill an array
-    """
-
-    arr = [float(inf)] * amnt
-    arr[0] = 0
-
-    return arr
+""" Making changes """
 
 
 def makeChange(coins, total):
-    """
-    Function to solve make change problem
-    """
+    """ Generate changes needed to reach total
 
-    if not coins:
-        return -1
-
+    Args:
+        coins ([List]): [List of Coins available]
+        total ([int]): [total amount needed]
+    """
     if total <= 0:
         return 0
-
-    coins.sort()
-    filled_arr = fill_arr(total)
-
-    for curr_amnt in range(len(filled_arr)):
-        for coin in coins:
-            if coin > curr_amnt:
-                break
-
-            curr_min_change = filled_arr[curr_amnt - coin] + 1
-            filled_arr[curr_amnt] = min(curr_min_change, filled_arr[curr_amnt])
-
-    if filled_arr[-1] == float(-1):
-        return -1
-
-    return filled_arr[-1]
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
